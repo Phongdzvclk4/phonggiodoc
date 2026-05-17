5d41761b8738aa6418554202b942bc4efc7c9bce
Phongdzvclk4 <phongndhe180390@fpt.edu.vn>
2026-05-17 16:06:25 +0700

    fix lab1

---
+++ hello_flutter_lab1/android/app/src/main/kotlin/com/lab1/hello_flutter_lab1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.lab1.hello_flutter_lab1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- lab1/android/app/src/main/kotlin/com/lab1/lab1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.lab1.lab1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
